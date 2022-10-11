21fd02d7a0770e85e7b18752606066c217254e70
minsoo822@naver.com <minsoo822@naver.com>
2022-10-11 16:49:24 +0900

    upload

---
+++ src/main/java/com/actorfw/infra/common/base/Bass.java
@@ -0,0 +1,104 @@
+package com.actorfw.infra.common.base;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class Bass {
+
+	
+//	uploaded
+	
+	private MultipartFile[] multipartFile;
+	
+	
+	private String seq;
+	private Integer type;
+	private Integer defaultNy;
+	private Integer sort;
+	private String path;
+	private String originalName;
+	private String uuidName;
+	private String ext;
+	private String size;
+	private Integer delNy;
+	private Integer pseq;
+	
+	
+	public MultipartFile[] getMultipartFile() {
+		return multipartFile;
+	}
+	public void setMultipartFile(MultipartFile[] multipartFile) {
+		this.multipartFile = multipartFile;
+	}
+	
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public Integer getType() {
+		return type;
+	}
+	public void setType(Integer type) {
+		this.type = type;
+	}
+	public Integer getDefaultNy() {
+		return defaultNy;
+	}
+	public void setDefaultNy(Integer defaultNy) {
+		this.defaultNy = defaultNy;
+	}
+	public Integer getSort() {
+		return sort;
+	}
+	public void setSort(Integer sort) {
+		this.sort = sort;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String getOriginalName() {
+		return originalName;
+	}
+	public void setOriginalName(String originalName) {
+		this.originalName = originalName;
+	}
+	public String getUuidName() {
+		return uuidName;
+	}
+	public void setUuidName(String uuidName) {
+		this.uuidName = uuidName;
+	}
+	public String getExt() {
+		return ext;
+	}
+	public void setExt(String ext) {
+		this.ext = ext;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public Integer getDelNy() {
+		return delNy;
+	}
+	public void setDelNy(Integer delNy) {
+		this.delNy = delNy;
+	}
+	public Integer getPseq() {
+		return pseq;
+	}
+	public void setPseq(Integer pseq) {
+		this.pseq = pseq;
+	}
+	
+	
+	
+	
+	
+}
