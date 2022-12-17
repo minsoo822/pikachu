56d8efcbb513fdf6981f04bb6f77b0d8113f1947
minsoo822@naver.com <minsoo822@naver.com>
2022-12-17 19:16:03 +0900

    수정

--- src/main/java/com/actorfw/infra/HomeController.java
+++ src/main/java/com/actorfw/infra/HomeController.java
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class BasicController {
+public class HomeController {
 
-        private static final Logger logger = LoggerFactory.getLogger(BasicController.class);
+        private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
         
         /**
          * Simply selects the home view to render by returning its name.
