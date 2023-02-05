e25b288ba2f1d7c12842756b17a6b2ad1147e4b3
minsoo822@naver.com <minsoo822@naver.com>
2023-02-05 15:37:48 +0900

    어노테이션 세로추가..

--- src/main/java/com/actorfw/infra/modules/code/CodeServiceImpl.java
+++ src/main/java/com/actorfw/infra/modules/code/CodeServiceImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service 
 public class CodeServiceImpl implements CodeService {
 
 	@Autowired
